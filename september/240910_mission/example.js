5192373433c6394774bb064d5688cbc34e2ee178
kkyung99 <becky0508@naver.com>
2024-09-17 00:46:59 +0900

    docs: 240910 mission

---
+++ september/240910_mission/example.js
@@ -0,0 +1,16 @@
+function calculateSum(a, b) {
+  return a + b;
+}
+
+const result = calculateSum(1, 2);
+console.log(result); // 3
+
+// 함수 정의
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+// 함수 호출
+const message = greet('Alice');
+console.log(message); // 출력: Hello, Alice!
+
